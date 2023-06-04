7ca6c93a3915a513449337eeb1e8806bbe606fff
conian <conian2505@gmail.com>
2023-06-04 17:44:29 +0700

    update UI new tweet form

---
+++ src/components/Modal/CreateTweetForm/CreateTweetForm.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import styles from "./createTweetForm.module.scss";
+type Props = {};
+
+const CreateTweetForm = (props: Props) => {
+  return <div>CreateTweetForm</div>;
+};
+
+export default CreateTweetForm;
